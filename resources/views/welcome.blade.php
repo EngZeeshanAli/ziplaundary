47be38ad3850e96a316cf9225cccc06f65075937
Zeeshan Ali <zeshan.lhr.pk@gmail.com>
2025-07-28 16:09:21 +0500

    working on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,39 +7,101 @@
     <title>Zip My Laundry</title>
     @include('widget.top_required')
 
-
 </head>
 <body class="antialiased">
 @include('widget.navbar')
 
-<div class="container-fluid app-bg-primary-color">
-    <div class="px-5">
-        <div class="row">
-            <div class="col-sm-12 col-md-6 d-flex align-items-center justify-content-center" style="height: 60vh;">
-                <div class="w-100">
-                    <div class="app-btn text-center p-2 text-white lato-regular f-16 w-50">
+<div class="container-fluid app-bg-primary-color py-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <!-- Text Section -->
+            <div class="col-12 col-md-6 mb-5 mb-md-0 d-flex justify-content-center">
+                <div class="text-center text-md-start">
+                    <div class="app-btn text-white lato-regular py-2 px-4 mb-3 d-inline-block">
                         Professional Laundry Service Company
                     </div>
-                    <h1 class="lato-regular f-72 fw-bold mt-3 mb-3">
+
+                    <h1 class="lato-regular f-72 fw-bold mb-3">
                         Laundry. Fresh.<br> Fast. To Your Door.
                     </h1>
-                    <span class="lato-regular f-18 text-gray-500 mt-3">
-            Book in seconds. We collect, clean, and deliver in 24–36 hours.
-        </span>
 
-                    <div class="row mt-4">
-                        <a href="#" class="btn btn-primary app-btn w-25 me-3 lato-regular">Book a Pickup</a>
-                        <a href="#" class="btn btn-primary app-btn-white w-25 lato-regular">How It Works</a>
+                    <p class="lato-regular f-18 text-gray-500 mb-4 text-gray">
+                        Book in seconds. We collect, clean, and deliver in 24–36 hours.
+                    </p>
+
+                    <div class="d-flex flex-column flex-sm-row justify-content-center justify-content-md-start">
+                        <a href="#" class="btn btn-primary app-btn me-sm-3 mb-3 mb-sm-0 lato-regular px-4 py-2">Book a
+                            Pickup</a>
+                        <a href="#" class="btn app-btn-white lato-regular px-4 py-2">How It Works</a>
                     </div>
                 </div>
             </div>
 
-            <div class="col-sm-12 col-md-6 d-flex justify-content-center">
-                <img class="img-fluid" src="{{ asset('images/header-girl.png') }}" alt="header">
+            <!-- Image Section -->
+            <div class="col-12 col-md-6 d-flex justify-content-center">
+                <img class="img-fluid" src="{{ asset('images/header-girl.png') }}" alt="header image">
             </div>
         </div>
     </div>
 </div>
+
+<div class="container">
+    <div class="col-lg-6 mt-5">
+        <h1 class="mt-5 lato-regular text-black f-48 fw-bold">How It Works</h1>
+        <span class="lato-regular f-18 text-gray">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lorem sagittis, proin ut lectus sed ut. Enim egestas enim id duis.
+            </span>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-lg-4 mt-5">
+            <div class="card p-3">
+                <img class="icon-40" src="{{ asset('images/calendar.png') }}"/>
+                <span class="mt-3 lato-regular fw-bold f-24">Schedule Pickup</span>
+                <span class="mt-2 lato-regular text-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Luctus cum purus.</span>
+                <span class="mt-2 lato-regular fw-semibold">Learn More
+                    <img class="icon-40 ms-3" src="{{ asset('images/pointer.png') }}"/></span>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mt-5">
+            <div class="card p-3">
+                <img class="icon-40" src="{{ asset('images/baskit.png') }}"/>
+                <span class="mt-3 lato-regular fw-bold f-24"> We Wash & Fold</span>
+                <span class="mt-2 lato-regular text-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Luctus cum purus.</span>
+                <span class="mt-2 lato-regular fw-semibold">Learn More
+                    <img class="icon-40 ms-3" src="{{ asset('images/pointer.png') }}"/></span>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mt-5">
+            <div class="card p-3">
+                <img class="icon-40" src="{{ asset('images/truck.png') }}"/>
+                <span class="mt-3 lato-regular fw-bold f-24">Delivery to Your Door</span>
+                <span class="mt-2 lato-regular text-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Luctus cum purus.</span>
+                <span class="mt-2 lato-regular fw-semibold">Learn More
+                    <img class="icon-40 ms-3" src="{{ asset('images/pointer.png') }}"/></span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid app-bg-primary-color py-5 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <span class="mt-3 lato-regular fw-bold f-48">Why Choose ZipMyLaundry?</span>
+                <span class="lato-regular f-16 text-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lorem sagittis, proin ut lectus sed ut. Enim egestas enim id duis.</span>
+                <div class="row">
+                    <img class="icon-40 col-2" src="{{ asset('images/calendar.png') }}"/>
+                    <div class="">
+                        <p class="mt-3 lato-regular fw-bold f-18">Why Choose ZipMyLaundry?</p>
+                        <span class="lato-regular f-16 text-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lorem sagittis, proin ut lectus sed ut. Enim egestas enim id duis.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @include('widget.footer_required')
 </body>
